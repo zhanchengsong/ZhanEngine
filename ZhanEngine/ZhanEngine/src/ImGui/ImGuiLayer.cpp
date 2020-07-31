723fb697e8be1f7409e0271defe31e382dbb51dd
Zhancheng Song <zhanchengdev@gmail.com>
2020-07-31 03:15:43 -0400

    Adding glfw impl for imgui and events

--- ZhanEngine/ZhanEngine/src/ImGui/ImGuiLayer.cpp
+++ ZhanEngine/ZhanEngine/src/ImGui/ImGuiLayer.cpp
@@ -1,9 +1,12 @@
 #include "zhpch.h"
 #include "ImGuiLayer.h"
 #include "imgui.h"
-#include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
+#include "Platform/OpenGL/imgui_impl_glfw.h"
+#include "Platform/OpenGL/imgui_impl_opengl3.h"
 #include "GLFW/glfw3.h"
 #include "Zhan/Application.h"
+#include "Zhan/Log.h"
+
 namespace Zhan {
 	ImGuiLayer::ImGuiLayer()
 		: Layer("ImGuiLayer")
@@ -45,6 +48,9 @@ namespace Zhan {
 		io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
 		io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
+		Application& app = Application::GetApplication();
+		GLFWwindow* window = static_cast<GLFWwindow*>(app.GetWindow().GetNativeWindow());
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
 		ImGui_ImplOpenGL3_Init("#version 410");
 	}
 	void ImGuiLayer::OnDetach()
@@ -52,14 +58,14 @@ namespace Zhan {
 	}
 	void ImGuiLayer::OnUpdate()
 	{
-		
+		//ZH_CORE_TRACE("ImGui onUpdate() called");
 		Application& app = Application::GetApplication();
 		ImGuiIO& io = ImGui::GetIO();
 		io.DisplaySize = ImVec2(app.GetWindow().getWidth(), app.GetWindow().getHeight());
 		float time = (float)glfwGetTime();
 		io.DeltaTime = m_Time > 0.0 ? (time - m_Time) : (1.0f / 60.0f);
 		m_Time = time;
-
+		ImGui_ImplGlfw_NewFrame();
 		ImGui_ImplOpenGL3_NewFrame();
 		ImGui::NewFrame();
 
@@ -68,8 +74,65 @@ namespace Zhan {
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	}
+	// Dispatch the event we received from GLFW (translated to our own)
+	// To the handlers in this layer 
+	// The dispatcher checks if the event should be dispatched (static type == templated type)
+	// Then it dispatces the event 
 	void ImGuiLayer::OnEvent(Event& event)
-	{
+	{	
+		ZH_CORE_TRACE("ImGui Received events: {0}", event.ToString());
+		EventDispatcher dispatcher(event);
+		dispatcher.Dispatch<MouseButtonPressedEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
+		dispatcher.Dispatch<MouseButtonReleasedEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
+		dispatcher.Dispatch<MouseMovedEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnMouseMovedEvent));
+		dispatcher.Dispatch<MouseScrollEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnMouseScrollEvent));
+		dispatcher.Dispatch<KeyPressedEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
+		dispatcher.Dispatch<KeyReleasedEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
+		dispatcher.Dispatch<WindowResizeEvent>(ZH_BIND_EVENT_FN(ImGuiLayer::OnWindowResizedEvent));
 
+	
+	}
+	// Process the event and set certain actions to be taken by imGui (GLFW -> Event -> ImGui actions)
+	bool ImGuiLayer::OnMouseButtonPressedEvent(MouseButtonPressedEvent& e)
+	{	
+		ZH_CORE_TRACE("OnMounseButtonPressedEvent handling");
+		ImGuiIO io = ImGui::GetIO(); // THis line gets io from current context
+		io.MouseDown[e.getMouseButton()] = true;
+		return false;
+	}
+	bool ImGuiLayer::OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e)
+	{
+		ImGuiIO io = ImGui::GetIO();
+		io.MouseDown[e.getMouseButton()] = false;
+		return false;
+	}
+	bool ImGuiLayer::OnMouseMovedEvent(MouseMovedEvent& e)
+	{
+		ZH_CORE_TRACE("OnMouseMovedEvent handling");
+		ImGuiIO io = ImGui::GetIO(); 
+		io.MousePos = ImVec2(e.getX(), e.getY());
+		return false;
+	}
+	bool ImGuiLayer::OnMouseScrollEvent(MouseScrollEvent& e)
+	{
+		ImGuiIO io = ImGui::GetIO();
+		io.MouseWheelH += e.getXOffSet();
+		io.MouseWheel += e.getYOffSet();
+		return false;
+	}
+	bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent& e)
+	{
+		return false;
+	}
+	bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent& e)
+	{
+		return false;
+	}
+	bool ImGuiLayer::OnWindowResizedEvent(WindowResizeEvent& e)
+	{
+		ImGuiIO io = ImGui::GetIO();
+		io.DisplaySize = ImVec2(e.getWidth(), e.getHeight());
+		io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
+		return false;
 	}
 }
--- ZhanEngine/ZhanEngine/src/ImGui/ImGuiLayer.h
+++ ZhanEngine/ZhanEngine/src/ImGui/ImGuiLayer.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "Zhan/Layer/Layer.h"
+#include "Zhan/Event/Event.h"
+#include "Zhan/Event/MouseEvent.h"
+#include "Zhan/Event/KeyEvent.h"
+#include "Zhan/Event/WindowEvent.h"
+
 namespace Zhan {
 	class ZHAN_API ImGuiLayer : public Layer {
 	public:
@@ -10,7 +15,15 @@ namespace Zhan {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
-
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrollEvent(MouseScrollEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		// bool OnKeyTypedEvent(KeyTypedEvent e);
+		bool OnWindowResizedEvent(WindowResizeEvent& e);
 	protected:
 		float m_Time = 0.0f;
 	};
